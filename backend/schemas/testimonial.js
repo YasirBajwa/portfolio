65a3df042c713955104bbd82fd0f2b66f9824880
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-06-26 10:05:51 +0500

    setup backend

---
+++ backend/schemas/testimonial.js
@@ -0,0 +1,32 @@
+
+export default {
+
+    name:'testimonial',
+    title:'Testimonial',
+    type:'document',
+    fields:[
+        {
+            name:'name',
+            title:'Name',
+            type:'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imageUrl',
+            title:'ImgUrl',
+            type:'image',
+            options:{
+                hotspot:true
+            }
+        }, 
+        {
+            name:'feedback',
+            title:'FeedBack',
+            type:'string'
+        }
+     ]
+}
