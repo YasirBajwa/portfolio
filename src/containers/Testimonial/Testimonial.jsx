4bd1568bcb0c543e53a8c862be6da3fd7bfce7b5
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-05 10:08:22 +0500

    fetch data from sanity in testimonial component

--- src/containers/Testimonial/Testimonial.jsx
+++ src/containers/Testimonial/Testimonial.jsx
@@ -3,19 +3,27 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
+import {HiChevronLeft,HiChevronRight} from 'react-icons/hi'
 import './Testimonial.scss';
 
 
 const Testimonial = () => {
+
   const [testimonial, setTestimonial] = useState([]);
+  const[brands,setBrands] = useState([]);
+  const [currentIndex,setCurrentIndex] = useState(0)
 
   useEffect(() => {
-    const query = '*[_type == "testimonial"]';
+    const query = '*[_type == "testimonials"]';
+    const brandQuery = '*[_type == "brands"]';
+
 
     client.fetch(query).then((data) => {
       setTestimonial(data);
     });
-
+    client.fetch(brandQuery).then((data) => {
+      setBrands(data);
+    });
   }, []);
 
 
@@ -26,3 +34,8 @@ const Testimonial = () => {
 }
 
 export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg',
+);
