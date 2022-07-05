4866357d24fa21f5918d4151b128e0ce1f59b807
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-05 10:02:44 +0500

    working testimonial section

--- src/containers/Testimonial/Testimonial.jsx
+++ src/containers/Testimonial/Testimonial.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 
 const Testimonial = () => {
+  const [testimonial, setTestimonial] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonial"]';
+
+    client.fetch(query).then((data) => {
+      setTestimonial(data);
+    });
+
+  }, []);
+
+
   return (
-    <div>Testimonial</div>
+    <>
+    </>
   )
 }
 
